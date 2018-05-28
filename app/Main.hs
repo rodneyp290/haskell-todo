90486219bcd62d2dd0cec5ba64dbb5778155f4eb
Rodney Pilgrim <rodneyp290@gmail.com>
2018-05-28 19:40:13 +1000

    ENHANCE: CLI Task parsing

--- app/Main.hs
+++ app/Main.hs
@@ -17,24 +17,43 @@ pointless = Task {
 
 data Usage = Usage {
                file :: FilePath
+               , tCmd :: TaskCommand
                }
+     deriving Show
 
 usage :: Parser Usage
-usage = Usage
-        <$> strOption
+usage = Usage <$> file <*> tCommand
+  where file = strOption
           (long "file"
           <> metavar "TODO-FILE"
           <> value "todo.json"
           <> help "JSON file containing the todo lists"
           )
+        tCommand = subparser $
+             cmd addP "add" "Add task to list"
+          <> cmd delP "delete" "Delete first matching task from list"
+          <> cmd comP "complete" "Complete first matching uncompleted task from list" 
+          <> cmd clrP "list" "Clear list"
+          <> cmd seeP "clear" "Clear list"
+        addP = Add <$> strArg "LIST" <*> strArg "TITLE" <*> strArg "DESC"
+        delP = Delete <$> strArg "LIST" <*> strArg "TITLE"
+        comP = Complete <$> strArg "LIST"<*> strArg "TITLE"
+        clrP = Clear <$> strArg "LIST"
+        seeP = See <$> strArg "LIST"
+        cmd p n d = command n (info (helper <*> p) (progDesc d))
+        strArg n = strArgument (metavar n)
 
 
 main :: IO ()
 main = do
-  todoFile <- pure.file =<< execParser (info (helper <*> usage)
+  execution <- execParser (info (helper <*> usage)
           ( fullDesc
           <> progDesc "CLI todo app"
           <> header "todo - a CLI todo app written in haskell" ))
+
+  let todoFile = file execution
+  print execution
+
   json <- BS.readFile todoFile
   let eLists = eitherDecode json :: Either String Lists
   case eLists of
--- src/Task.hs
+++ src/Task.hs
@@ -1,21 +1,34 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Task where 
 
+import Data.String
 import Data.Text
 import Data.HashMap.Strict
 import Data.Aeson
 import Data.Aeson.TH
 
+newtype LName = LName Text deriving (Show, Eq, IsString, ToJSON, FromJSON)
+newtype TName = TName Text deriving (Show, Eq, IsString, ToJSON, FromJSON)
+newtype TDesc = TDesc Text deriving (Show, Eq, IsString, ToJSON, FromJSON)
+
 data Task = Task 
-  { title :: Text
-  , desc  :: Text
+  { title :: TName
+  , desc  :: TDesc
   , done  :: Bool
   } deriving (Show,Eq)
 
 type Lists = HashMap Text [Task]
 
-addTask :: Lists -> Text -> Task -> Lists
-addTask current list task = insertWith (++) list [task] current
+data TaskCommand = Add LName TName TDesc
+                 | Delete LName TName
+                 | Complete LName TName
+                 | Clear LName
+                 | See LName
+      deriving (Show)
+
+addTask :: Lists -> LName -> Task -> Lists
+addTask current (LName list) task = insertWith (++) list [task] current
 
 $(deriveJSON defaultOptions ''Task ) -- "Haskell Macro" / "Template Haskell"
 -- TIP: put templates at bottom of code
